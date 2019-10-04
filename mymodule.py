deb8cf33b16dab0cfae169b70ba272679f61810f
Suguru <suu.rockfield@gmail.com>
2019-10-04 20:46:02 +0900

    bag fixed

--- mymodule.py
+++ mymodule.py
@@ -41,7 +41,7 @@ def get_config_json(filename, sample_json):
     return config_json
 
 # Slackに出力
-class SlackAPI(object, inifile_name):
+class SlackAPI(object):
 
     def __init__(self, inifile_name):
         sample_json = {'URL': {'webhook_url': None}}
